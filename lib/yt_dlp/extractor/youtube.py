b4f0c0bab2173bcfab89fe8256c53f71ffcf1ee8
lekma <lekmalek@gmail.com>
2024-08-15 09:18:07 +0100

    add initRange and indexRange to yt formats

--- lib/yt_dlp/extractor/youtube.py
+++ lib/yt_dlp/extractor/youtube.py
@@ -4056,6 +4056,8 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
                 'language_preference': PREFERRED_LANG_VALUE if is_default else -10 if is_descriptive else -1,
                 # Strictly de-prioritize broken, damaged and 3gp formats
                 'preference': -20 if is_broken else -10 if is_damaged else -2 if itag == '17' else None,
+                'initRange': fmt.get('initRange'),
+                'indexRange': fmt.get('indexRange'),
             }
             mime_mobj = re.match(
                 r'((?:[^/]+)/(?:[^;]+))(?:;\s*codecs="([^"]+)")?', fmt.get('mimeType') or '')
